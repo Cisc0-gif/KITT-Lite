73a32c76cad041ff2a5e250ae8483092d6493bfc
Cisc0-gif <53851069+Cisc0-gif@users.noreply.github.com>
2019-12-30 20:06:09 -0800

    Update KITT_INSTALLER.sh

--- KITT_INSTALLER.sh
+++ KITT_INSTALLER.sh
@@ -3,13 +3,13 @@ read -p "*RUN AS ROOT*" root
 echo "[*]Running lib installer..."
 ./lib_install.sh
 echo "[+]Done!"
-echo "[*]Moving KITT_lite directory to /opt..."
+echo "[*]Moving KITT-Lite directory to /opt..."
 mkdir /opt
 cd ..
-mv KITT_lite /opt
+mv KITT-Lite /opt
 echo "[+]Done!"
 echo "[*]Writing KITT_lite.py to alias..."
-alias KITTlite="python3 /opt/KITT_lite/KITT_lite.py"
+alias KITTlite="python3 /opt/KITT-Lite/KITT_lite.py"
 echo "[+]Done!"
 echo 'Execute "KITTlite" to run KITT Lite Framework'
 
