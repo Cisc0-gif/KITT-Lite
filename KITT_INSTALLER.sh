22520ae1b9d46605c52f55f7cbf9f575a5252bd6
root <root@raspberrypi>
2020-02-17 12:42:50 -0800

    dir fix

--- KITT_INSTALLER.sh
+++ KITT_INSTALLER.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 read -p "*RUN AS ROOT*" root
 echo "[*]Writing File Permissions..."
-chmod -R 777 *
+sudo chmod -R 777 *
 echo "[*]Done!"
 echo "[*]Running lib installer..."
-./lib_install.py
+sudo ./lib_install.py
 echo "[+]Done!"
 echo "[*]Moving KITT-Lite directory to /opt..."
-mkdir /opt
+sudo mkdir /opt
 cd ..
-mv KITT-Lite /opt
+sudo mv KITT-Lite /opt
 echo "[+]Done!"
 echo "[*]Writing KITT_lite.py to alias..."
-echo "alias KITTlite='python3 /opt/KITT-Lite/KITT_lite.py'" >> ~/.bashrc
+sudo echo "alias KITTlite='python3 /opt/KITT-Lite/KITT_lite.py'" >> /home/kali/.bashrc
 echo "[+]Done!"
 echo 'Execute "KITTlite" to run KITT Lite Framework'
 
